19faa84b31e7a982ec7eb3074c4fc0e1d96e64d7
Ruk <rrerusu@gmail.com>
2021-01-08 16:44:16 -0600

    Understanding why we put the <Predicate<String>> infront of the Stream.

--- src/org/erusu/codewars/kyu_4/CatchingCarMileageNumbers/CatchingCarMileageNumbers.java
+++ src/org/erusu/codewars/kyu_4/CatchingCarMileageNumbers/CatchingCarMileageNumbers.java
@@ -1,5 +1,9 @@
 package org.erusu.codewars.kyu_4.CatchingCarMileageNumbers;
 
+//import java.util.Arrays;
+//import java.util.function.Predicate;
+//import java.util.stream.Stream;
+
 import java.lang.StringBuilder;
 
 public class CatchingCarMileageNumbers {
@@ -81,28 +85,26 @@ public class CatchingCarMileageNumbers {
 		return false;
 	}
 
-	/*
-	 *  VERY Interesting Way: Lambda - Predicate Stream
-	 *
-	 *  import java.util.Arrays;				// Arrays.stream in line 97
-	 *	import java.util.function.Predicate;	// Lambda Function at line 92
-	 *	import java.util.stream.Stream;			// Stream to return stream at line 93 for .anyMatch() boolean return at line 99
-	 *
-	 *  // create stream of lambda functions
-	 *  public static boolean isReallyInteresting(int number, final int[] awesomePhrases) {
-	 *		return Stream.<Predicate<String>>of(
-	 *				s -> s.matches("\\d0+"),										// test for each digit to be the same: s -> s.matcher("(\\d)+")
-	 *				s -> new StringBuilder(s).reverse().toString().equals(s),
-	 *				s -> "1234567890".contains(s),
-	 *				s -> "9876543210".contains(s),
-	 *				s -> Arrays.stream(awesomePhrases).anyMatch(n -> parseInt(s) == n)
-	 *		).anyMatch( p -> number > 99 && p.test(Integer.toString(number)));	// p is a predicate function, test is defined in Predicate interface
-	 *	  }
-	 *
-	 *	  public static int isInteresting(int number, int[] awesomePhrases) {
-	 *		return isReallyInteresting(number, awesomePhrases) ? 2 :
-	 *			   isReallyInteresting(number + 1, awesomePhrases) ? 1 :
-	 *			   isReallyInteresting(number + 2, awesomePhrases) ? 1 : 0;
-	 *	  }
-	 */
+//	    // VERY Interesting Way: Lambda - Predicate Stream
+//
+////	    import java.util.Arrays;				// Arrays.stream in line 97
+////	 	import java.util.function.Predicate;	// Lambda Function at line 92
+////	 	import java.util.stream.Stream;			// Stream to return stream at line 93 for .anyMatch() boolean return at line 99
+//
+//	    // create stream of lambda functions
+//	    public static boolean isReallyInteresting(int number, final int[] awesomePhrases) {
+//	 		return Stream.<Predicate<String>>of(										// Specifically state <Predicate<String>> because it is not implicit
+//	 				s -> s.matches("\\d0+"),										// test for each digit to be the same: s -> s.matcher("(\\d)+")
+//	 				s -> new StringBuilder(s).reverse().toString().equals(s),
+//	 				s -> "1234567890".contains(s),
+//	 				s -> "9876543210".contains(s),
+//	 				s -> Arrays.stream(awesomePhrases).anyMatch(n -> Integer.parseInt(s) == n)
+//	 		).anyMatch( p -> number > 99 && p.test(Integer.toString(number)));	// p is a predicate function, test is defined in Predicate interface
+//	 	  }
+//
+//	 	  public static int isInteresting(int number, int[] awesomePhrases) {
+//	 		return isReallyInteresting(number, awesomePhrases) ? 2 :
+//	 			   isReallyInteresting(number + 1, awesomePhrases) ? 1 :
+//	 			   isReallyInteresting(number + 2, awesomePhrases) ? 1 : 0;
+//	 	  }
 }
